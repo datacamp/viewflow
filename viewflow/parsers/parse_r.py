147e5b851eb7b39ab6e65f656f385f775591e1ed
Pieter Vanslambrouck <pieter.vanslambrouck@datacamp.com>
2021-07-16 11:47:07 +0200

    edit parsers for r and sql, bugfixes

--- viewflow/parsers/parse_r.py
+++ viewflow/parsers/parse_r.py
@@ -6,12 +6,13 @@ from typing import Any, Dict
 
 def parse_r(file: pathlib.Path) -> Dict[str, Any]:
     content = file.read_text().split("\n")
-    l = [i for i, x in enumerate(content) if re.search(r"#[ \t]*?---", x)]
-    yml = "\n".join(content[l[0] + 1 : (l[1])])
+    l = [i for i, x in enumerate(content) if re.search(r"# ---", x)]
+    temp = "\n".join(content[l[0] + 1 : (l[1])])
+    yml = re.sub(r"# ", "", temp)
     task_config = yaml.safe_load(yml)
     extras = {
         "type": "ROperator",
-        "content": "\n".join(content[0 : (l[0] - 1)] + content[(l[1] + 2) :]),
+        "content": "\n".join(content[0 : max(l[0]-1,0)] + content[(l[1] + 2) :]),
         "task_file_path": str(file),
     }
     task_config.update(extras)
