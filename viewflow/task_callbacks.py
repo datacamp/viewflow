7ad621cecf833e9c91fc3e4499865851ddb99567
Pieter Vanslambrouck <pieter.vanslambrouck@datacamp.com>
2021-07-26 14:51:27 +0200

    edit email callback

--- viewflow/task_callbacks.py
+++ viewflow/task_callbacks.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow.utils.email import send_email
-
+from textwrap import dedent
 
 def success_callback_default(airflow_context):
     pass
@@ -16,19 +16,16 @@ def failure_callback_default(airflow_context):
 
 def failure_callback_email(airflow_context):
     """Send an email to the owner of the failed task.
-    Note that you first have to configure the email settings in $AIRFLOW_HOME/airflow.cfg"""
-    info = {
-        "task": airflow_context.get("task_instance").task_id,
-        "dag": airflow_context.get("task_instance").dag_id,
-        "owner": airflow_context["task"].owner,
-        "execution_time": airflow_context.get("execution_date"),
-        "URL_logs": {airflow_context.get("task_instance").log_url}
-    }
-    
-    recipient_emails=[airflow_context["task"].owner]
+    Note that you first have to configure the email (SMTP) settings in $AIRFLOW_HOME/airflow.cfg"""
 
-    title = ''
-    body = ''
+    title = f"""Airflow failed task: {airflow_context.get("task_instance").task_id}"""
+    body = dedent(f"""
+    DAG:            {airflow_context.get("task_instance").dag_id}
+    Task:           {airflow_context.get("task_instance").task_id}
+    Owner:          {airflow_context.get("task").owner}
+    Execution time: {airflow_context.get("execution_date")}
+    URL logs:       {airflow_context.get("task_instance").log_url}
+    """)
 
+    recipient_emails = [airflow_context["task"].owner]
     send_email(recipient_emails, title, body)
-
